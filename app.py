938ad1d73957c82c348f4d745383e0d0ffa92b9d
ZKDev01 <rdromaguera2001@gmail.com>
2024-05-08 11:15:56 -0400

    add embedding model

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-import os 
+import os
+import textwrap 
 import dotenv
+
 import PIL.Image
+
+import re
+import tqdm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 import google.generativeai as genai
+import google.ai.generativelanguage as glm
 
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 dotenv.load_dotenv()
 telegram_token = os.getenv("telegram_token") 
@@ -13,6 +29,53 @@ geminiapi_key = os.getenv("geminiapi_key")
 
 genai.configure(api_key=geminiapi_key)
 model = genai.GenerativeModel('gemini-pro')
+model_embedding = 'models/embedding-001'
+
+original = os.getcwd() + "\\database\\doc"
+dirs = os.listdir(original)
+
+database = []
+for dir in dirs:
+  with open(original + '\\' + dir, encoding='utf-8') as file:
+    result = file.read()
+  database.append((dir, result))
+
+def embed(title, text):
+  return genai.embed_content(
+    model=model_embedding,
+    content=text,
+    task_type="retrieval_document",
+    title=title)["embedding"]
+
+df = pd.DataFrame(database)
+df.columns = ['Title', 'Text']
+df['Embeddings'] = df.apply(lambda row: embed(row['Title'], row['Text']), axis=1)
+
+# df.to_json(os.getcwd() + "\\database\\database.json")
+
+def find_best_passage(query, dataframe):
+  query_embedding = genai.embed_content(
+    model=model_embedding,
+    content=query,
+    task_type="retrieval_query")
+  dot_products = np.dot(np.stack(dataframe['Embeddings']), query_embedding["embedding"])
+  idx = np.argmax(dot_products)
+  return dataframe.iloc[idx]['Text']
+
+def make_prompt(query, relevant_passage: str):
+  escaped = relevant_passage.replace("'", "").replace('"', "").replace("\n", " ")
+  prompt = textwrap.dedent(
+    """Eres un bot útil e informativo que responde preguntas utilizando texto del pasaje de referencia incluido a continuación \
+    Asegúrate de responder incluyendo toda la información relevante. \
+    Asegúrate de desglosar conceptos complicaados y mantener un tono amigable y conversacional \
+    Si el pasaje no es relevante para la respuesta no respondas la pregunta \
+    QUESTION: '{query}'
+    PASSAGE: '{relevant_passage}'
+
+    ANSWER:
+    """).format(query=query, relevant_passage=escaped)
+
+  return prompt
 
 # commands
 async def start(update: Update, context: ContextTypes):
@@ -20,23 +83,17 @@ async def start(update: Update, context: ContextTypes):
 async def help(update: Update, context: ContextTypes):
   await update.message.reply_text("Hola mucho gusto, en que puedo ayudarte?")
 
-# respuesta a los mensajes 
 def handle_response(text: str, context: ContextTypes, update: Update):
-  proccesed_text = text.lower()
+  query = text.lower()
+  passage = find_best_passage(query, df)
+  
+  prompt = make_prompt(query, passage)
+  answer = model.generate_content(prompt)
 
-  response = model.generate_content([proccesed_text])
-  response.resolve()
-
-  return response.text
-  """
-  if 'hola' in proccesed_text:
-    return
-  else:
-    return 'No entender your language'
-  """
+  return answer.text
 
 async def handle_message(update: Update, context: ContextTypes):
-  message_type = update.message.chat.type # private, group, subgroup, channel
+  message_type = update.message.chat.type 
   text = update.message.text
 
   if message_type == 'group':
@@ -55,18 +112,12 @@ async def error(update: Update, context: ContextTypes):
   await update.message.reply_text("Ha ocurrido un error")
 
 if __name__ == '__main__':
-  # creamos la app
   app = Application.builder().token(telegram_token).build() 
 
-  # creamos commnads
   app.add_handler(CommandHandler('start', start))
   app.add_handler(CommandHandler('help', help))
 
-  # creamos respuestas
   app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
-  # creamos errores
   app.add_error_handler(error)
-
-  # iniciar bot
   app.run_polling(poll_interval=1, timeout=3)

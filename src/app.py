2a7996945c0dd7f2260470cf471b72fde651a6ab
ZKDev01 <rdromaguera2001@gmail.com>
2024-05-27 22:34:25 -0400

    new code with a db

--- src/app.py
+++ src/app.py
@@ -3,4 +3,5 @@
 2. Almacenarla en un dataframe, que el codigo base creado de busqueda esta implementado con dataframe
 3. Mejorar la busqueda para que no solo recolepte un solo chunk, sino un valor k que se puede escoger
 4. Mejorar los prompts a como dijo el profe
+5. Mejorar el query
 """
